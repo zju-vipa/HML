82609aaeee35504b130f6d0c63bf3ca0ef473d54
zyh1999 <1007108223@qq.com>
2021-11-23 22:02:32 +0800

    just a  pipe-multiprocess test

--- HML/celery_tasks/algorithms/_DimReduction_utils/TEST.py
+++ HML/celery_tasks/algorithms/_DimReduction_utils/TEST.py
@@ -2,16 +2,16 @@ from sklearn.decomposition import PCA
 import pandas as pd
 import numpy as np
 from celery_tasks.algorithms._DimReduction_utils import Test2
-def XXX():
-    class a():
-        def __init__(self, v):
-            self.a = v
-    p=a(10)
-    print(Test2.K(p))
-    print("zyh")
-def main():
-    XXX()
-    print("XXX")
-    print("yyy")      
+from celery_tasks.algorithms._DimReduction_utils.Test2 import f
+from multiprocessing import Process, Pipe
+
+
 if __name__ == '__main__':
-    main()
+    conn_1, conn_2 = Pipe()  # 生成管道的两边，分别传给两个进程
+    p = Process(target=f, args=(conn_1,))
+    p.start()
+    print("Conn_2: ", conn_2.recv())
+    print("Conn_2: ", conn_2.recv())
+    conn_2.send("Data_1 from Conn_2")
+    p.join()
+    print("主")
